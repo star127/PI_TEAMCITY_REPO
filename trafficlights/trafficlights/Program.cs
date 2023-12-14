f2fea6834fdea1d8cc3e02622cd37952853cf614
Артур <75448169+star127@users.noreply.github.com>
2023-12-14 18:17:44 +0300

    Add files via upload

---
+++ trafficlights/trafficlights/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace trafficlights
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Красный");     // Сигнал "Красный"
+                Thread.Sleep(5000);               // Задержка 5 секунд
+
+                Console.WriteLine("Желтый");     // Сигнал "Желтый"
+                Thread.Sleep(2000);               // Задержка 2 секунды
+
+                Console.WriteLine("Зеленый");    // Сигнал "Зеленый"
+                Thread.Sleep(5000);               // Задержка 5 секунд
+
+                Console.WriteLine("Желтый");     // Сигнал "Желтый"
+                Thread.Sleep(2000);               // Задержка 2 секунды
+            }
+        }
+    }
+}
